9f6ed045d18e42d7406741db02254fe3c4fc96be
Noob <aaziz9642@gmail.com>
2021-10-29 00:20:44 +0600

    added

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -17,7 +17,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
             product: data._id,
             name: data.name,
             image: data.image,
-            price: data.price,
+            price: data.price - ((data.price * data.offer_percentage) / 100),
             countInStock: data.countInStock,
             qty
         }
