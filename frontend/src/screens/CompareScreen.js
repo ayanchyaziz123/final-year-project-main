f569cf2f9152651f4db9a9216058cddc24a0196f
Noob <aaziz9642@gmail.com>
2021-11-26 01:43:30 +0600

    edited copmare product

--- frontend/src/screens/CompareScreen.js
+++ frontend/src/screens/CompareScreen.js
@@ -40,8 +40,12 @@ function CompareScreen(props){
 
             {prod && (
                 <Row className="mt-2 mb-5">
+                    
+                    <Col md={2}>
+                                <h2>This is compare product</h2>
+                    </Col>
                     <Col md={5}>
-                          <Card className="bg-dark text-white p-2">
+                      
                               
                                     {props.location.state.lep1 && prod.map((pr, index) => pr._id == props.location.state.lep1 ? (
                         <div key={index}>
@@ -51,20 +55,20 @@ function CompareScreen(props){
                            <Card.Body>
                                 <h5 className="text-white">{pr.name}</h5>
                                 <p>Ram {pr.ram_memory}</p>
+                                <hr></hr>
+                                                <p>Storage {pr.storage}</p>
                                 <p>{ReactHtmlParser(pr.description)}</p>
                                 <p>{pr.price}</p>
                            </Card.Body>
 
                         </div>
                     ) : null )}
-                    </Card>
+                   
                     </Col>
-                    <Col md={2}>
-                        <h1 className="text-center mt-5">VS</h1>
-                    </Col>
+                
 
                     <Col md={5}>
-                        <Card className="bg-dark text-white p-2">
+                     
                                     {props.location.state.lep2 && prod.map((pr, index) => pr._id == props.location.state.lep2 ? (
                             <div key={index}>
                                 <Link to={`/product/${pr._id}`}>
@@ -80,7 +84,7 @@ function CompareScreen(props){
 
                             </div>
                         ) : null)}
-                        </Card>
+               
                     </Col>
 
                 </Row>
